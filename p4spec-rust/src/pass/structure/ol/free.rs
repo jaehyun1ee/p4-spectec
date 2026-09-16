7501a1595eae70384877882fbb5cb9b45ed140ca
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2026-09-17 00:08:00 +0900

    perf(structure): accumulate free identifiers

--- p4spec-rust/src/pass/structure/ol/free.rs
+++ p4spec-rust/src/pass/structure/ol/free.rs
@@ -9,93 +9,97 @@ use super::ast::*;
 // - Instructions
 
 impl Free for InstrKind {
-    fn free(&self) -> IdSet {
+    fn free_into(&self, free: &mut IdSet) {
         match self {
-            Self::If(instr) => instr.free(),
-            Self::Hold(instr) => instr.free(),
-            Self::Case(instr) => instr.free(),
-            Self::Group(instr) => instr.free(),
-            Self::Let(instr) => instr.free(),
-            Self::Rule(instr) => instr.free(),
-            Self::Result(instr) => instr.free(),
-            Self::Return(instr) => instr.free(),
-            Self::Debug(instr) => instr.free(),
+            Self::If(instr) => instr.free_into(free),
+            Self::Hold(instr) => instr.free_into(free),
+            Self::Case(instr) => instr.free_into(free),
+            Self::Group(instr) => instr.free_into(free),
+            Self::Let(instr) => instr.free_into(free),
+            Self::Rule(instr) => instr.free_into(free),
+            Self::Result(instr) => instr.free_into(free),
+            Self::Return(instr) => instr.free_into(free),
+            Self::Debug(instr) => instr.free_into(free),
         }
     }
 }
 
 impl Free for IfInstr {
-    fn free(&self) -> IdSet {
-        self.exp.free().union(self.block.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.exp.free_into(free);
+        self.block.free_into(free);
     }
 }
 
 impl Free for HoldInstr {
-    fn free(&self) -> IdSet {
-        self.not_exp
-            .free()
-            .union(self.block_hold.free())
-            .union(self.block_not_hold.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.not_exp.free_into(free);
+        self.block_hold.free_into(free);
+        self.block_not_hold.free_into(free);
     }
 }
 
 impl Free for CaseInstr {
-    fn free(&self) -> IdSet {
-        self.exp.free().union(self.cases.as_slice().free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.exp.free_into(free);
+        self.cases.as_slice().free_into(free);
     }
 }
 
 impl Free for GroupInstr {
-    fn free(&self) -> IdSet {
-        self.exps.as_slice().free().union(self.block.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.exps.as_slice().free_into(free);
+        self.block.free_into(free);
     }
 }
 
 impl Free for LetInstr {
-    fn free(&self) -> IdSet {
-        self.exp_l
-            .free()
-            .union(self.exp_r.free())
-            .union(self.block.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.exp_l.free_into(free);
+        self.exp_r.free_into(free);
+        self.block.free_into(free);
     }
 }
 
 impl Free for RuleInstr {
-    fn free(&self) -> IdSet {
-        self.not_exp.free().union(self.block.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.not_exp.free_into(free);
+        self.block.free_into(free);
     }
 }
 
 impl Free for ResultInstr {
-    fn free(&self) -> IdSet {
-        self.exps.as_slice().free()
+    fn free_into(&self, free: &mut IdSet) {
+        self.exps.as_slice().free_into(free);
     }
 }
 
 impl Free for ReturnInstr {
-    fn free(&self) -> IdSet {
-        self.exp.free()
+    fn free_into(&self, free: &mut IdSet) {
+        self.exp.free_into(free);
     }
 }
 
 impl Free for DebugInstr {
-    fn free(&self) -> IdSet {
-        self.exp.free().union(self.instr.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.exp.free_into(free);
+        self.instr.free_into(free);
     }
 }
 
 // - Case analysis
 
 impl Free for Case {
-    fn free(&self) -> IdSet {
-        self.guard.free().union(self.block.free())
+    fn free_into(&self, free: &mut IdSet) {
+        self.guard.free_into(free);
+        self.block.free_into(free);
     }
 }
 
 // - Blocks
 
 impl Free for Block {
-    fn free(&self) -> IdSet {
-        self.as_slice().free()
+    fn free_into(&self, free: &mut IdSet) {
+        self.as_slice().free_into(free);
     }
 }
