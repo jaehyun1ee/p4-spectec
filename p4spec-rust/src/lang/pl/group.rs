5ca406686b833290a105d0118c4f9bdfdfef21b8
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2026-09-19 21:47:36 +0900

    feat(pl): add prose traversal helpers

---
+++ p4spec-rust/src/lang/pl/group.rs
@@ -0,0 +1,80 @@
+//! Rule-group extraction from prose dispatch blocks
+
+use super::{annot, ast::*};
+
+/// A borrowed rule group extracted from a dispatch block
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct RuleGroup<'a> {
+    pub hints: &'a annot::Hints,
+    pub id_rulegroup: &'a Id,
+    pub id_rel: &'a Id,
+    pub rel_signature: &'a RelSignature,
+    pub exps: &'a [Exp],
+    pub body: &'a BlockGroup,
+}
+
+/// Collects nested rule groups in depth-first source order
+pub fn collect_groups(block: &BlockDispatch) -> Vec<RuleGroup<'_>> {
+    let mut rule_groups = Vec::new();
+    collect_groups_from_block(block, &mut rule_groups);
+    rule_groups
+}
+
+fn collect_groups_from_block<'a>(block: &'a BlockDispatch, rule_groups: &mut Vec<RuleGroup<'a>>) {
+    for instr in block {
+        collect_groups_from_instr(instr, rule_groups);
+    }
+}
+
+fn collect_groups_from_instr<'a>(
+    instr: &'a Instr<InstrDispatch>,
+    rule_groups: &mut Vec<RuleGroup<'a>>,
+) {
+    match &instr.node.node {
+        InstrKind::If(IfInstr { block, .. }) => collect_groups_from_block(block, rule_groups),
+        InstrKind::Hold(HoldInstr { hold_case, .. }) => match hold_case {
+            HoldCase::Both(block_hold, block_not_hold) => {
+                collect_groups_from_block(block_hold, rule_groups);
+                collect_groups_from_block(block_not_hold, rule_groups);
+            }
+            HoldCase::Hold(block, _) | HoldCase::NotHold(block, _) => {
+                collect_groups_from_block(block, rule_groups);
+            }
+        },
+        InstrKind::Case(CaseInstr { cases, .. }) => {
+            for case in cases {
+                collect_groups_from_block(&case.block, rule_groups);
+            }
+        }
+        InstrKind::Let(..) | InstrKind::Debug(_) | InstrKind::Destruct(..) => {}
+        InstrKind::CheckLetSub(CheckLetSubInstr { block, .. })
+        | InstrKind::CheckLetMatch(CheckLetMatchInstr { block, .. })
+        | InstrKind::OptionGet(OptionGetInstr { block, .. }) => {
+            collect_groups_from_block(block, rule_groups);
+        }
+        InstrKind::Tier(TierInstr {
+            tier: InstrDispatch::Route(RouteDispatchInstr { blocks }),
+        }) => {
+            for block in blocks {
+                collect_groups_from_block(block, rule_groups);
+            }
+        }
+        InstrKind::Tier(TierInstr {
+            tier:
+                InstrDispatch::Group(GroupDispatchInstr {
+                    id_rel,
+                    id_group,
+                    rel_signature,
+                    exps_input,
+                    block,
+                }),
+        }) => rule_groups.push(RuleGroup {
+            hints: &instr.hints,
+            id_rulegroup: id_group,
+            id_rel,
+            rel_signature,
+            exps: exps_input,
+            body: block,
+        }),
+    }
+}
---
+++ p4spec-rust/tests/lang/pl/partial.rs
@@ -0,0 +1,133 @@
+use p4spec_rust::{
+    lang::common::source::{Position, Span},
+    lang::{common::notation::mixfix::Mixfix, hints::input::InputHint, il, pl},
+};
+
+fn span(name: &str) -> Span {
+    Span::new(Position::new(name, 0, 0), Position::new(name, 0, 0))
+}
+
+fn id(name: &str) -> il::ast::Id {
+    p4spec_rust::phrase! { node: name.to_owned(), span: span(name) }
+}
+
+fn typ() -> il::ast::Typ {
+    p4spec_rust::phrase! { node: il::ast::TypKind::Bool, span: span("type") }
+}
+
+fn exp(exp_kind: pl::ast::ExpKind) -> pl::ast::Exp {
+    pl::annot::Annotated {
+        node: p4spec_rust::note_phrase! {
+            node: exp_kind,
+            note: il::ast::TypKind::Bool,
+            span: span("expression"),
+        },
+        hints: pl::annot::Hints::default(),
+    }
+}
+
+fn variable(name: &str) -> pl::ast::Exp {
+    exp(pl::ast::ExpKind::Var(id(name)))
+}
+
+fn call(name: &str) -> pl::ast::Exp {
+    exp(pl::ast::ExpKind::Call(id(name), Vec::new(), Vec::new()))
+}
+
+fn group_instr(
+    instr_kind: pl::ast::InstrKind<pl::ast::InstrGroup>,
+) -> pl::ast::Instr<pl::ast::InstrGroup> {
+    pl::annot::Annotated {
+        node: p4spec_rust::note_phrase! {
+            node: instr_kind,
+            note: None,
+            span: span("instruction"),
+        },
+        hints: pl::annot::Hints::default(),
+    }
+}
+
+#[test]
+fn test_expression_and_path_classification_finds_recursive_calls() {
+    let exp_nested = exp(pl::ast::ExpKind::Tuple(vec![exp(pl::ast::ExpKind::Upd(
+        Box::new(variable("base")),
+        Box::new(p4spec_rust::note_phrase! {
+            node: pl::ast::PathKind::Idx(
+                Box::new(p4spec_rust::note_phrase! {
+                    node: pl::ast::PathKind::Root,
+                    note: il::ast::TypKind::Bool,
+                    span: span("root"),
+                }),
+                Box::new(call("index")),
+            ),
+            note: il::ast::TypKind::Bool,
+            span: span("path"),
+        }),
+        Box::new(variable("field")),
+    ))]));
+
+    assert!(pl::partial::is_partial_exp(&exp_nested));
+    assert!(!pl::partial::is_partial_exp(&variable("plain")));
+}
+
+#[test]
+fn test_guard_classification_checks_only_evaluated_expressions() {
+    assert!(pl::partial::is_partial_guard(&pl::ast::Guard::Cmp(
+        il::ast::CmpOp::Bool(p4spec_rust::lang::xl::bool::CmpOp::Eq),
+        il::ast::OpTyp::Bool,
+        call("comparison"),
+    )));
+    assert!(pl::partial::is_partial_guard(&pl::ast::Guard::CheckLetSub(
+        typ(),
+        Box::new(il::ast::Subcheck::Skip),
+        call("binding"),
+    )));
+    assert!(!pl::partial::is_partial_guard(&pl::ast::Guard::Mem(call("membership"))));
+}
+
+#[test]
+fn test_instruction_classification_distinguishes_leaf_and_nested_partiality() {
+    let instr_nested_call = group_instr(pl::ast::InstrKind::Tier(pl::ast::TierInstr {
+        tier: pl::ast::InstrGroup::Return(pl::ast::ReturnGroupInstr { exp: call("nested") }),
+    }));
+    let instr_branch = group_instr(pl::ast::InstrKind::If(pl::ast::IfInstr {
+        exp: variable("condition"),
+        iter_exps: Vec::new(),
+        block: vec![instr_nested_call],
+        dangle: false,
+    }));
+    let instr_shorthand = group_instr(pl::ast::InstrKind::OptionGet(pl::ast::OptionGetInstr {
+        exp_l: variable("left"),
+        exp_r: variable("right"),
+        block: Vec::new(),
+    }));
+
+    assert!(!pl::partial::is_partial_instr(pl::partial::is_partial_instr_group, &instr_branch,));
+    assert!(pl::partial::is_partial_instr(pl::partial::is_partial_instr_group, &instr_shorthand,));
+}
+
+#[test]
+fn test_tier_classification_uses_rule_arguments_not_rule_kind() {
+    assert!(!pl::partial::is_partial_instr_group(&pl::ast::InstrGroup::Rule(
+        pl::ast::RuleGroupInstr {
+            id: id("rule"),
+            not_exp: Mixfix::Arg(variable("argument")),
+            input_hint: InputHint::new(vec![0]),
+            iter_instrs: Vec::new(),
+        }
+    )));
+    assert!(pl::partial::is_partial_instr_group(&pl::ast::InstrGroup::Rule(
+        pl::ast::RuleGroupInstr {
+            id: id("rule"),
+            not_exp: Mixfix::Arg(call("argument")),
+            input_hint: InputHint::new(vec![0]),
+            iter_instrs: Vec::new(),
+        }
+    )));
+    assert!(!pl::partial::is_partial_instr_group(&pl::ast::InstrGroup::Backtrack(
+        pl::ast::BacktrackGroupInstr { blocks: vec![Vec::new()] }
+    )));
+    assert!(!pl::partial::is_partial_instr_dispatch(&pl::ast::InstrDispatch::Route(
+        pl::ast::RouteDispatchInstr { blocks: Vec::new() }
+    )));
+}
