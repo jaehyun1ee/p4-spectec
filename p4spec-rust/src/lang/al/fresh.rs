ec7ff1aa1bd4317f7c935a78f454033506b1b8bc
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2026-09-19 23:57:25 +0900

    fix(fresh): retain requested alias spans

--- p4spec-rust/src/lang/al/fresh.rs
+++ p4spec-rust/src/lang/al/fresh.rs
@@ -34,7 +34,8 @@ fn var_from_typ(menv: &MEnv, span: &Span, typ: &Typ) -> Var {
         typ.syntax_eq(typ_alias) && typ_name.as_str() != id_alias.node.as_str()
     });
     if let (Some((id_alias, typ_alias)), None) = (vars_alias.next(), vars_alias.next()) {
-        return Var { id: id_alias.clone(), typ: typ_alias.clone(), iters: vec![] };
+        let id = crate::phrase! { node: id_alias.node.clone(), span: span.clone() };
+        return Var { id, typ: typ_alias.clone(), iters: vec![] };
     }
 
     match &typ.node {
