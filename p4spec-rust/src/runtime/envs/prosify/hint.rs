eb04bcfcc8a24835002c20daadd5ed56a25393d8
jaehyun1ee <99jaehyunlee@kaist.ac.kr>
2026-09-21 17:55:10 +0900

    refactor(prose): make hint environment a newtype

--- p4spec-rust/src/runtime/envs/prosify/hint.rs
+++ p4spec-rust/src/runtime/envs/prosify/hint.rs
@@ -12,38 +12,36 @@ enum HintKey {
 }
 
 #[derive(Debug, Default)]
-pub struct HEnv {
-    hints_by_key: BTreeMap<HintKey, Hints>,
-}
+pub struct HEnv(BTreeMap<HintKey, Hints>);
 
 impl HEnv {
     pub fn insert_case(&mut self, id_typ: &Id, mixop: &Mixop, hints: Hints) {
         let key = HintKey::Case(id_typ.node.clone(), mixop.clone());
-        self.hints_by_key.insert(key, hints);
+        self.0.insert(key, hints);
     }
 
     pub fn insert_func(&mut self, id_func: &Id, hints: Hints) {
         let key = HintKey::Func(id_func.node.clone());
-        self.hints_by_key.insert(key, hints);
+        self.0.insert(key, hints);
     }
 
     pub fn insert_rel(&mut self, id_rel: &Id, hints: Hints) {
         let key = HintKey::Rel(id_rel.node.clone());
-        self.hints_by_key.insert(key, hints);
+        self.0.insert(key, hints);
     }
 
     pub fn get_case(&self, id_typ: &Id, mixop: &Mixop) -> Option<&Hints> {
         let key = HintKey::Case(id_typ.node.clone(), mixop.clone());
-        self.hints_by_key.get(&key)
+        self.0.get(&key)
     }
 
     pub fn get_func(&self, id_func: &Id) -> Option<&Hints> {
         let key = HintKey::Func(id_func.node.clone());
-        self.hints_by_key.get(&key)
+        self.0.get(&key)
     }
 
     pub fn get_rel(&self, id_rel: &Id) -> Option<&Hints> {
         let key = HintKey::Rel(id_rel.node.clone());
-        self.hints_by_key.get(&key)
+        self.0.get(&key)
     }
 }
